27f9537a49b89f1fb793b2a6eca10f7864720916
Niyamul Hasan <niyamulhasan301@gmail.com>
2025-08-09 22:13:59 +0600

    Added 2 dimentional array

---
+++ array.array.js
@@ -0,0 +1,23 @@
+const tournament_runs= [[], [], []];
+const exam_marks = [
+    [34, 345, 45, 67, 78],
+[12, 23, 34, 45, 56],
+[78, 89, 90, 12, 23],
+ [5, 15, 25, 35, 45],
+[100, 200, 300, 400, 500]
+
+]
+
+// console.log(exam_marks[0])
+const first_class_marks = exam_marks[0]
+// console.log(first_class_marks[0])
+// console.log(exam_marks[0][0])
+exam_marks[0][1] = 66
+exam_marks[1].pop()
+exam_marks[1].push[44]
+// console.log(exam_marks)
+
+
+for(const marks of exam_marks){
+    console.log(marks)
+}
