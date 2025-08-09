120c9106aa503fc5dbbac13009b97c1b15195829
Niyamul Hasan <niyamulhasan301@gmail.com>
2025-08-09 23:17:13 +0600

    Added recape.js

---
+++ recape.js
@@ -0,0 +1,25 @@
+/*
+*
+*/  
+
+const books = ['physis', 'javascript', 'css', 'math'];
+for (let i = 0; i < books.length; i++) {
+    console.log(books[i]);
+}
+
+// revarse
+const rev_books = books.reverse();
+const sorted_books = books.sort();
+
+const gadgets =[
+    {name: 'laptop', price: 34000, color:'black', brand: 'lenovo'},
+    {name: 'android phone', price: 23300, color: 'meron', brand: samsung}
+]
+
+// 2d array
+const matrix = [
+    [3,4,6],
+    [4,7,8],
+    [5,8,9]
+
+]
